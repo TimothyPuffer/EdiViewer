0a4b76e011407b57f7234a8408d51676f72e7f52
Timothy Puffer Work <timothypuffer@gmail.com>
2013-12-26 18:09:46 -0600

    work

--- EDIViewer/EdiFileLine.cs
+++ EDIViewer/EdiFileLine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace EDIViewer
 {
@@ -36,14 +37,83 @@ namespace EDIViewer
                 return RawText.Split(_separator, StringSplitOptions.None);
             }
         }
+
+        public string[] TextWithSeparator
+        {
+            get
+            {
+                return Zip(Text, Separator.First().ToString()).ToArray();
+            }
+        }
+
+        private IEnumerable<T> Zip<T>(IEnumerable<T> ttt,T tt)
+        {
+            bool notFirst = false;
+            foreach(T tttt in ttt)
+            {
+                if (notFirst)
+                {
+                    yield return tt;
+                }
+                notFirst = true;
+                yield return tttt;
+            }
+        }
         public string RawText { get; private set; }
     }
 
     public class EdiFileLineShower
     {
+        IEnumerable<EdiFileLine> _lines;
         public EdiFileLineShower(IEnumerable<EdiFileLine> lines)
         {
-
+            _lines = lines;
         }
+
+        private class InternalSelectedString :ISelectedString, INotifyPropertyChanged
+        {
+
+            public InternalSelectedString(string value)
+            {
+                Value = value;
+            }
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string Value
+            {
+                get;
+                private set;
+            }
+
+            bool _isSelected;
+            public bool IsSelected
+            {
+                get
+                {
+                    return _isSelected;
+                }
+                set
+                {
+                    _isSelected = value;
+                    RaisePropertyChanged("IsSelected");
+                }
+            }
+
+            private void RaisePropertyChanged(string caller)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(caller));
+                }
+            }
+        }
+    }
+
+    public interface ISelectedString
+    {
+        string Value { get; }
+        bool IsSelected { get; set; }
     }
 }
